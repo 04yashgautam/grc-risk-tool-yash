ee91657e412a3c686de244b7c2a20637c497eb1f
Yash Gautam <2004yashgautam@gmail.com>
2026-02-06 14:27:46 +0530

    improved backend acc to NIST framework

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import or_
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
@@ -50,6 +51,7 @@ class RiskOutput(RiskInput):
     id: int
     score: int
     level: str
+    mitigation: str
 
     class Config:
         from_attributes = True
@@ -82,17 +84,28 @@ def calculate_risk_level(score: int) -> str:
     if score <= 18: return "High"
     return "Critical"
 
+# mititgation
+def get_mitigation_hint(level: str) -> str:
+    if level == "Low":
+        return "Accept / monitor"
+    elif level == "Medium":
+        return "Plan mitigation within 6 months"
+    elif level == "High":
+        return "Prioritize action + compensating controls (NIST PR.AC)"
+    elif level == "Critical":
+        return "Immediate mitigation required + executive reporting"
+    return ""
+
 
 # route /assess-risk
 @app.post("/assess-risk", response_model=RiskOutput)
 def assess_risk(risk: RiskInput, db: Session = Depends(get_db)):
 
-
     # business logic
     score = risk.likelihood * risk.impact 
-    
-
     level = calculate_risk_level(score)   
+    mitigation = get_mitigation_hint(level)
+
 
     new_risk = RiskModel(
         asset=risk.asset,
@@ -100,7 +113,8 @@ def assess_risk(risk: RiskInput, db: Session = Depends(get_db)):
         likelihood=risk.likelihood,
         impact=risk.impact,
         score=score,
-        level=level
+        level=level,
+        mitigation=mitigation
     )
 
     db.add(new_risk)
@@ -111,11 +125,20 @@ def assess_risk(risk: RiskInput, db: Session = Depends(get_db)):
 
 # route /risks
 @app.get("/risks", response_model=List[RiskOutput])
-def get_risks(level: Optional[str] = None, db: Session = Depends(get_db)):
+def get_risks(level: Optional[str] = None, search: Optional[str] = None, db: Session = Depends(get_db)):
     
     query = db.query(RiskModel)
     
     if level:
         query = query.filter(RiskModel.level == level)
+
+    if search:
+        search_fmt = f"%{search}%"
+        query = query.filter(
+            or_(
+                RiskModel.asset.ilike(search_fmt),
+                RiskModel.threat.ilike(search_fmt)
+            )
+        )
         
     return query.all()
